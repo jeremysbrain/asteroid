1682595e0f91d4181219b7d9a9585bc78af6893a
Jeremy Brainard <jeremysbrain@outlook.com>
2025-03-23 20:39:39 -0700

    updated pygame loop

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import pygame
-import constants
+
+from constants import *
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    pygame.get_init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        screen.fill(color="black")
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+    #print("Starting Asteroids!")
+    #print(f"Screen width: {SCREEN_WIDTH}")
+    #print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
