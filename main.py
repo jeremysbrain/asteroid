2b1f206a02aec6bf2df5dbfaf029bc5e124f17ee
Jeremy Brainard <jeremysbrain@outlook.com>
2025-03-24 07:44:57 -0700

    add rotation with a/d keys

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 import pygame
-
 from constants import *
-from player import *
+from player import Player
+
 
 def main():
-    pygame.get_init()
-    clock = pygame.time.Clock()
-    dt = 0
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    p = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
+        p.update(dt)
+
         screen.fill(color="black")
-
-        p = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
         p.draw(screen)
-
         pygame.display.flip()
-        clock.tick(60)
+
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
